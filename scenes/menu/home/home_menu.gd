5db7d069618a25d22fdc97fa887819a86a55e9d8
Mathys DC <rokassd9@gmail.com>
2025-04-21 19:50:25 +0200

    Make credits scene

--- scenes/menu/home/home_menu.gd
+++ scenes/menu/home/home_menu.gd
@@ -19,7 +19,7 @@ func _on_settings_pressed() -> void:
 	print("Settings")
 
 func _on_credits_pressed() -> void:
-	print("Credits")
+	get_tree().change_scene_to_file("res://scenes/menu/credits/credits.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
