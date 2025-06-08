060fcc2c13b4a3e56c1ecf95c6a7e82994edc027
Mathys DC <rokassd9@gmail.com>
2025-06-08 18:18:39 +0200

    Npc Name look at player

--- objects/npc/npc.gd
+++ objects/npc/npc.gd
@@ -3,8 +3,8 @@ extends CharacterBody3D
 @onready var area: Area3D = $Area3D
 @onready var label: Label3D = $Label3D
 @export var npc_name: String = "CACA"
+var player_camera: Camera3D = null
 
-# Dialogue de test
 var dialogue_lines: Array[String] = [
 	"Salut, aventurier !",
 	"Bienvenue dans le monde de Godoria.",
@@ -15,17 +15,23 @@ func _ready():
 	label.text = npc_name
 	area.body_entered.connect(_on_body_entered)
 	area.body_exited.connect(_on_body_exited)
+	label.visible = false
+
+func _process(delta):
+	if player_camera and label.visible:
+		label.look_at(player_camera.global_transform.origin, Vector3.UP)
+		label.rotation.y += deg_to_rad(180)
 
 func _on_body_entered(body):
 	if body.is_in_group("players") and body.is_multiplayer_authority():
-		label.show()
+		label.visible = true
 		body.set_current_npc(self)
+
+		if body.get_node("CameraController").has_node("Camera3D"):
+			player_camera = body.get_node("CameraController").get_node("Camera3D")
 
 func _on_body_exited(body):
 	if body.is_in_group("players") and body.is_multiplayer_authority():
-		label.hide()
+		label.visible = false
 		body.set_current_npc(null)
-
-
-func get_dialogue_lines():
-	return dialogue_lines
+		player_camera = null
