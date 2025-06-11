932cf329af27d62518b67a00501b300fdf386f0b
LuDoSniper <lucien.donnarieix@hesias.fr>
2025-06-11 10:43:15 +0200

    Full use of regional servers

--- scripts/MULTIPLAYER.gd
+++ scripts/MULTIPLAYER.gd
@@ -1,10 +1,12 @@
 extends Node
 
-const DEFAULT_ADDRESS := "daoh.donnarieix.fr"
 
 # Par défaut il est à 3642 pour le debug local
 # Attention, cela veux donc dire qu'en local on ne peux se connecter qu'à Europe.
 # Le problème sera réglé lorsque tout les serveur dédiés seront déployés
+var DEFAULT_ADDRESS_BEGIN := "daoh-"
+var DEFAULT_ADDRESS_END := ".donnarieix.fr"
+var REGION := "eu"
 var LISTEN_PORT := 3642
 var MAX_CHARACTER_COUNT := 4
 var token := ""
@@ -27,7 +29,7 @@ func create_server(peer_connected: Callable) -> void:
 	#peer_connected.call()
 	
 func join_server() -> void:
-	peer.create_client("ws://" + DEFAULT_ADDRESS + ":" + str(LISTEN_PORT))
+	peer.create_client("ws://" + DEFAULT_ADDRESS_BEGIN + REGION + DEFAULT_ADDRESS_END + ":" + str(LISTEN_PORT))
 	multiplayer.multiplayer_peer = peer
 
 func get_server_by_id(id: int) -> Dictionary:
