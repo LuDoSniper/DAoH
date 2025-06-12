60ee1029cfad614a93fcd759951ef501ec967097
Mathys DA CUNHA <mathys.da-cunha@hesias.fr>
2025-06-12 15:33:58 +0200

    Separer le code en 2 scenes

--- objects/menu/class/button_picker.gd
+++ objects/menu/class/button_picker.gd
@@ -1,25 +1,14 @@
 extends Button
 
 @onready var label: Label = $PanelContainer/VBoxContainer/Label
-@onready var skins: Node3D = $Skins
+@onready var texture_rect: TextureRect = $PanelContainer/VBoxContainer/TextureRect
 
 func _ready():
-	for child in get_tree().get_nodes_in_group(""):
-		print(child.name, " -> ", child)
-
-	hide_skin()
 	label.text = ""
-
-func hide_skin(show_skin = null) -> void:
-	for child in skins.get_children():
-		if child is Node3D and not(child.name == show_skin):
-			child.hide()
-		else:
-			child.show()
 
 func set_player_name(name: String) -> void:
 	print(name)
 	label.text = name
 
-func set_player_skin(player_class: String) -> void:
-	hide_skin(player_class)
+func set_viewport_texture(texture: Texture2D) -> void:
+	texture_rect.texture = texture
