1fadc97432a018520f67a4027d21e7e6acd30ca8
unknown <elpiou2@gmail.com>
2025-06-14 15:30:50 +0200

    fix son d'ambiance ig

--- scenes/world/world.gd
+++ scenes/world/world.gd
@@ -10,6 +10,7 @@ extends Node3D
 
 func _ready():
 	print("I'M READY !")
+	$AudioStreamPlayer3D.play()
 	if has_meta("server") and get_meta("server"):
 		MULTIPLAYER.create_server(init)
 		UTILS.print_local(self, "I'M THE SERVER")
